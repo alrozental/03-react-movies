c0dd52fc688e58a14f2e0880018ff75273b9c21b
Alina Rozental <dev.alinaro@gmail.com>
2026-03-30 11:09:58 +0200

    feat: add axios and modern-normalize dependencies; refactor App component and remove unused styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
-import App from './components/App.module.css/App.tsx';
+import App from './components/App/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
